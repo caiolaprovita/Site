f4500485d3250d4baedd13d57559f913a75f2c7f
caiolaprovita <caiolaprovita@gmail.com>
2026-04-09 21:48:04 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -112,7 +112,7 @@ export default function SiteInstitucional() {
             <img
               src="/logo-cbl.png"
               alt="CBL Soluções"
-              className="h-14 w-auto object-contain"
+              className="h-20 w-auto object-contain"
             />
             <div>
               <div
